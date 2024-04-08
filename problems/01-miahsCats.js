68322039eb207424f78c4a9dee5d98dc8f99f023
damonryon <damon.ryon@gmail.com>
2024-04-07 20:02:16 -0700

    01 solved

--- problems/01-miahsCats.js
+++ problems/01-miahsCats.js
@@ -23,9 +23,33 @@ miahsCats(obj3) // 3
 ***********************************************************************/
 
 const miahsCats = obj => {
-    // Your code here 
+    let values = Object.values(obj)
+    let keys = Object.keys(obj)
+    let count = 0
+
+    for(let val of values){
+       val = val.toLowerCase()
+       if(val.includes('cute')) count++
+       if(val.includes('adorable')) count++
+    }
+
+    for(let key of keys){
+        key = key.toLowerCase()
+        if(key.includes('cute')) count++
+        if(key.includes('adorable')) count++
+    }
+    return count 
 }
 
+
+obj1 = {"Finn": "Smart lil boy, maximum cuTe", "Jake": "Not a dog, still cUte"}
+miahsCats(obj1) // 2
+
+obj2 = {"CuteFace": "Has an adoRable face, loves snuggles", "Baby": "Very chonky lady, adorable fluffy face"}
+miahsCats(obj2) // 3
+
+obj3 = {"Princess": "So much sass, cute AND fluffy", "adorable-boy": "Cute and funny"}
+miahsCats(obj3) // 3
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try{
     module.exports = miahsCats;
